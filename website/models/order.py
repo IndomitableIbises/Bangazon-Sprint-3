85428eb4b37a7e9e97539ff5ec70a78e41b5531d
sirwin4 <sean.irwinxc@gmail.com>
2018-08-20 10:46:36 -0500

    first commit

---
+++ website/models/order.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.urls import reverse
+from . import models
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.PROTECT)
+    price = models.DateField(blank=True, null=True)
+    shopping_cart = models.ManyToManyField(Product, on_delete=models.PROTECT)

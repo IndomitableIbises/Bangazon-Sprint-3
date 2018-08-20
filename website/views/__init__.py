c854e17be067c1ba61876ee304e934a176d092e8
pzc <paul.zimmerman.clayton@gmail.com>
2018-08-20 10:57:09 -0500

    modularized the shit outta this

---
+++ website/views/__init__.py
@@ -0,0 +1,6 @@
+from .index import index
+from .list_products import list_products
+from .login_user import login_user
+from .register import register
+from .sell_product import sell_product
+from .user_logout import user_logout

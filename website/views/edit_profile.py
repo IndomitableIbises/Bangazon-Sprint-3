49eb45bc782e420d0224fea3a5d53f6b93a4dc58
pzc <paul.zimmerman.clayton@gmail.com>
2018-08-21 13:31:12 -0500

    commented out irrelevant stuff, just check the model

--- website/views/edit_profile.py
+++ website/views/edit_profile.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
+# from django.shortcuts import render
+# from django.contrib.auth.models import User
 
-def edit_profile(request):
-    user_data = User.objects.all()
-    return render(request, 'edit_profile.html', {'user_data': user_data})
+# def edit_profile(request):
+#     user_data = User.objects.all()
+#     return render(request, 'edit_profile.html', {'user_data': user_data})

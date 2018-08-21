c8f6f90e0733829d38ee473981345a1a8afdfc73
eagobert <eagobert@hotmail.com>
2018-08-21 13:47:43 -0500

    Finish faker_factory.py

--- website/management/commands/faker_factory.py
+++ website/management/commands/faker_factory.py
@@ -8,11 +8,17 @@ from website.models import User, Profile, Category, Product, Order, Payment
 class Command(BaseCommand):
 
   def handle(self, *args, **options):
-    seeder.add_entity(User, 5) # the number argument is the total num of rows you want created
+    """ 
+    This function seeds the website database:
+      *args = database table name
+      **options = # of rows to create in table
+    """
+
+    seeder.add_entity(User, 5)
     seeder.add_entity(Profile, 5)
     seeder.add_entity(Category, 5)
     seeder.add_entity(Product, 25)
-    # Override the seeder "guess" of what faker provider you want it to use
-    seeder.add_entity(Training, 20)
+    seeder.add_entity(Order, 20)
+    seeder.add_entity(Payment, 20)
 
     inserted_pks = seeder.execute()

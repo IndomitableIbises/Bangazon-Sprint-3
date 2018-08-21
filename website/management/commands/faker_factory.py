73cd718c829fe17b140da80ce0c4796560ba6e33
eagobert <eagobert@hotmail.com>
2018-08-21 15:31:49 -0500

    make changes to faker data

--- website/management/commands/faker_factory.py
+++ website/management/commands/faker_factory.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from django_seed import Seed
 seeder = Seed.seeder()
 import random
-from website.models import User, Profile, Category, Product, Order, Payment
+from website.models import User, Profile, Category, Product, Payment #, Order
 
 class Command(BaseCommand):
 
@@ -18,7 +18,7 @@ class Command(BaseCommand):
     seeder.add_entity(Profile, 5)
     seeder.add_entity(Category, 5)
     seeder.add_entity(Product, 25)
-    seeder.add_entity(Order, 20)
+    #seeder.add_entity(Order, 20)
     seeder.add_entity(Payment, 20)
 
     inserted_pks = seeder.execute()

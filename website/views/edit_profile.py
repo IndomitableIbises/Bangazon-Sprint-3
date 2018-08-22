5ce0aa3f00df9c5f79a8539b2d58695d9679f88c
pzc <paul.zimmerman.clayton@gmail.com>
2018-08-22 13:34:35 -0500

    got user address and phone as default value in profile form

--- website/views/edit_profile.py
+++ website/views/edit_profile.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from website.forms import ProfileForm, UserForm
+from django.contrib.auth.models import User
 
 
 @login_required
 def edit_profile(request):
     if request.method == "GET":
+        u = User.objects.get(id=request.user.id)
         # Here's where we load the form
         user_form = UserForm(initial={'first_name': request.user.first_name,
         'last_name': request.user.last_name},) # create user field and set defaults from currently signed in user
-        profile_form = ProfileForm(initial={'address': request.user,
-        'phone': 123},) # create profile filds and set defaults. NEED TO MAKE THESE 
+        profile_form = ProfileForm(initial={'address': u.profile.address,
+        'phone': u.profile.phone},) # create profile filds and set defaults. NEED TO MAKE THESE 
         return render(request, 'edit_profile.html', {
         'user_form': user_form,
         'profile_form': profile_form,

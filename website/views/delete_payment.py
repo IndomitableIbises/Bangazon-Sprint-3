16e3be46dbeb0c03066b47aa93e60e56c86ab412
sirwin4 <sean.irwinxc@gmail.com>
2018-08-24 11:30:24 -0500

    payment delete added

--- website/views/delete_payment.py
+++ website/views/delete_payment.py
@@ -2,13 +2,17 @@ from django.http import HttpResponseRedirect
 from website.models import Payment
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext
+from django.contrib.auth.models import User
 
 @login_required
 def delete_payment(request, pk):
-
-        payment = Payment.objects.get(pk=pk)
-        if request.user == payment.user:
-                payment.delete()
-                return HttpResponseRedirect("/payment")
-        else:
-                return HttpResponseForbidden()
+    """Displays template for deleting a payment
+    Sean Irwin"""
+    payment = get_object_or_404(Payment, pk=pk)
+    payment.active = False
+    payment.save()
+    return HttpResponseRedirect('/')
